a1374c7f114206a8db6e5870dec365d9135bf8c3
XeFrostz <116756996+XeFrostz@users.noreply.github.com>
2025-04-26 02:23:30 +0700

    Update stylelist.lua

--- stylelist.lua
+++ stylelist.lua
@@ -1,1 +1,47 @@
-
+return {
+    Jackpot = {
+        { Name = "LuckyBounce", RequiresBall = true, RequiresAwakening = false }, -- Ability 1 (C)
+        { Name = "BigWin", RequiresBall = true, RequiresAwakening = false },     -- Ability 2 (V)
+        { Name = "Jackpot", RequiresBall = true, RequiresAwakening = true }      -- Ability 3 (B)
+    },
+    Phantom = {
+        { Name = "Misdirection", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Fadeaway", RequiresBall = true, RequiresAwakening = false },
+        { Name = "PhantomStep", RequiresBall = true, RequiresAwakening = true }
+    },
+    Star = {
+        { Name = "SpotUp", RequiresBall = true, RequiresAwakening = false },
+        { Name = "StarPower", RequiresBall = true, RequiresAwakening = false },
+        { Name = "StarBurst", RequiresBall = true, RequiresAwakening = true }
+    },
+    Sniper = {
+        { Name = "DeadEye", RequiresBall = true, RequiresAwakening = false },
+        { Name = "SnipeShot", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Lockdown", RequiresBall = true, RequiresAwakening = true }
+    },
+    Inferno = {
+        { Name = "BlazeStep", RequiresBall = true, RequiresAwakening = false },
+        { Name = "FireSpin", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Inferno", RequiresBall = true, RequiresAwakening = true }
+    },
+    Cyber = {
+        { Name = "TechDrive", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Glitch", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Overclock", RequiresBall = true, RequiresAwakening = true }
+    },
+    Dynasty = {
+        { Name = "RoyalGuard", RequiresBall = true, RequiresAwakening = false },
+        { Name = "EmperorStep", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Dynasty", RequiresBall = true, RequiresAwakening = true }
+    },
+    Vortex = {
+        { Name = "SpinCycle", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Whirlwind", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Vortex", RequiresBall = true, RequiresAwakening = true }
+    },
+    Mystic = {
+        { Name = "Illusion", RequiresBall = true, RequiresAwakening = false },
+        { Name = "Mirage", RequiresBall = true, RequiresAwakening = false },
+        { Name = "MysticAura", RequiresBall = true, RequiresAwakening = true }
+    }
+}
